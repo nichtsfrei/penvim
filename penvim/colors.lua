8d8c38d7c9f20653a88762a2228b15d8c30a6e74
Philipp Eder <philipp.eder@greenbone.net>
2023-07-20 16:26:26 +0200

    nix update

--- penvim/colors.lua
+++ penvim/colors.lua
@@ -2,13 +2,13 @@ require('rose-pine').setup({
     disable_background = true
 })
 
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+function ColorMyPencils(color)
+    color = color or "rose-pine-moon"
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd.colorscheme(color)
 
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
